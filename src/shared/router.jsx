5ae9cc98233c6a80fc66c5887f4d32f9943f9583
NickYOOO <outgrowthr@gmail.com>
2022-08-20 09:23:54 +0900

    #6 NotFound 경로추가

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
-
+import NotFound from "../pages/NotFound"
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="*" element={<NotFound/>} />
     </Routes>
   );
 };
