9df5888cc5df3bc30408472bd3923e1411d2a6e7
Dusuna <dusunax@gmail.com>
2022-08-21 00:31:14 +0900

    404페이지 중앙 정렬, fadeout

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,12 +5,13 @@ import styled from "styled-components"
 const NotFound = () => {
     const navigate = useNavigate("")
     const goBack = () => navigate("/")
-    setTimeout(goBack,2000)
+    setTimeout(goBack, 2000)
     
     return (
         <div>
             <FullPage onClick={goBack}>
-            <h1 style={{color: "white"}}>알수 없는 페이지 입니다.</h1>
+                <h1>알 수 없는 요청입니다.</h1>
+                <h3>이전 페이지로 이동합니다.</h3>
             </FullPage>
         </div >
     );
@@ -20,6 +21,23 @@ export default NotFound;
 
 const FullPage = styled.div`
     position: fixed;
-    top:0; right: 0; bottom: 0; left: 0;
-    background: linear-gradient(to top, #fc257e, #fcdf7e)
+    top:0; 
+    right: 0; 
+    bottom: 0; 
+    left: 0;
+
+    background: linear-gradient(to top, #fc257e, #fcdf7e);
+    color: #fff;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+
+    animation: fadeout .2s 1.8s forwards;
+
+    @keyframes fadeout {
+        0% {opacity: 1}
+        100% {opacity: 0}
+    }
 `
