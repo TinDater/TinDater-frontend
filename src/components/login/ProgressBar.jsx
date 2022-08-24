8326daf845e22547db41087c2be739a6b16c5159
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-08-24 15:52:26 +0900

    #40 프로그레스바 시작

---
+++ src/components/login/ProgressBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+function ProgressBar(props) {
+    const {signNumber} = props
+   console.log(props)
+    return (
+   <ProgressBar>
+    <HighLight width={(signNumber / 7) * 100 + "%"}/>
+   </ProgressBar>
+  )
+}
+
+const progressBar = styled.div`
+    background: #eee;
+    width: 100%;
+    height: 40px;
+
+`;
+
+const HighLight = styled.div`
+    background: orange;
+    width ${(props) => props.width};
+    height: 40px;
+`;
+export default ProgressBar;
